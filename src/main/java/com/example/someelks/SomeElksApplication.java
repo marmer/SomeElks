cba0cc192758488b26b9c9332cce62b8252af9f0
mariano.mertinat <Mariano.Mertinat@gematik.de>
2022-04-29 17:10:35 +0200

    Initial commit

---
+++ src/main/java/com/example/someelks/SomeElksApplication.java
@@ -0,0 +1,13 @@
+package com.example.someelks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeElksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeElksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/someelks/SomeElksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.someelks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeElksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
